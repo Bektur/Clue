7ee512aa9097bd83091cbfc813c76f09c338e5ab
Bektur <tgbeko@gmail.com>
2017-04-30 14:03:27 -0700

    Added dynamic variable disprovedCard and handSize

--- clue.pl
+++ clue.pl
@@ -8,6 +8,11 @@
 %6 weapons 
 %9 rooms 
 
+% maybe create dynamic varaibles for which cards does the character have
+:- dynamic handSize/2.
+:- dynamic knownCards/2
+:- dynamic disprovedCards/2.
+
 
 % List of all possible characters
 character(mustard).
@@ -38,21 +43,21 @@ room(study).
 
 
 getUserInput(X):-
-	write('Choose a player'),
+	write("Choose a player"),
 	nl, 
 	read(X),
 	getSuspects(X).
 
 getWhoSuggest(X):-
-	write("Who made the Accusation"),
+	write("Who made the Suggestion"),
 	nl,
 	read(X).
 
 
-getAccusation(Accusation,Accuser,Disprover):-
-	getWhoSuggest(Accuser),
-	character(Accuser),	
-	write("Enter a suggession in the format Weapon, suspect, room: "),
+getSuggestion(Suggestion,Suggester,Disprover):-
+	getWhoSuggest(Suggester),
+	character(Suggester),	
+	write("Enter a suggession in the format weapon, suspect, room: "),
 	nl, 
 	write("What was the weapon"),
 	nl,
@@ -66,18 +71,24 @@ getAccusation(Accusation,Accuser,Disprover):-
 	nl,
 	read(Room),
 	room(Room),
-	Accusation = [Weapon,Suspect,Room],
-	write("Who disproved the Accusation, if no one disproves it, enter the name of the Accuser"),
+	Suggestion = [Weapon,Suspect,Room],
+	write("Who disproved the Suggestion, if no one disproves it, enter the name of the Suggester"),
 	nl, 
 	read(Disprover),
-	character(Disprover).
+	character(Disprover),
+	write("Write your card to disprove"),
+	nl, 
+	read(Card),
+	assert(disprovedCards(Disprover,Card).
 
-getAccusation(Accusation,Accuser):-
+
+
+getAccusatio(Accusation,Accuser):-
 	write("Who made an incorrect accussation"),
 	nl,
 	read(Accuser),
 	character(Accuser),	
-	write("Enter the incorrect accusation in the format weapon, suspect, room: "),
+	write("Enter the incorrect Suggestion in the format weapon, suspect, room: "),
 	nl, 
 	write("What was the weapon"),
 	nl,
@@ -93,6 +104,16 @@ getAccusation(Accusation,Accuser):-
 	room(Room),
 	Accusation = [Weapon,Suspect,Room].
 
+getHands(0,_).
+getHands(NHands,Character):-
+	write("Enter your cards"),
+	nl, 
+	read(Hands),
+	assert(handSize(Character,Hands)),
+	assert(disprovedCard(Character,Hands)),
+	N1 is NHands - 1,
+	getHands(N1,Character).
+
 initGame(Characters,Hands):-
 	write("Enter the number of players"),
 	nl,
@@ -105,6 +126,7 @@ initGame(Characters,Hands):-
 	write("How many cards are in your hand?"),
 	nl, 
 	read(H1),
+	getHands(H1,C1),
 	Characters = [C1|CT],
 	Hands = [H1|HT],
 	N1 is NPlayers - 1,
@@ -123,11 +145,14 @@ playerInfo(NPlayers,[CH|CT],[NH|NT]):-
 	read(NH),
 	N1 is NPlayers - 1,
 	playerInfo(N1,CT,NT).
-	
 
+
+min(A, B, Min) :- 
+	A < B -> Min = A; 
+	Min = B. 
 
 menuScreen(X):-
-	write("Press 1 to record a Accusation"),
+	write("Press 1 to record a Suggestion"),
 	nl,
-	write("Press 2 to record an accusation").
+	write("Press 2 to record an Suggestion").
 
