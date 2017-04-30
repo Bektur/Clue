bb4a89f2a327ab79c58437688804566d7702d037
Russell <raluttrell@ucdavis.edu>
2017-04-30 13:39:33 -0700

    added numbers. fixed suggestion

--- clue.pl
+++ clue.pl
@@ -17,6 +17,13 @@ character(white).
 character(plum).
 character(peacock).
 
+character(1).
+character(2).
+character(3).
+character(4).
+character(5).
+character(6).
+
 % List of all possible weapons
 weapon(knife).
 weapon(candlestick).
@@ -24,6 +31,13 @@ weapon(rope).
 weapon(revolver).
 weapon(wrench).
 weapon(pipe).
+
+weapon(7).
+weapon(8).
+weapon(9).
+weapon(10).
+weapon(11).
+weapon(12).
 
 % List of all possible rooms
 room(kitchen).
@@ -36,6 +50,15 @@ room(lounge).
 room(hall).
 room(study).
 
+room(13).
+room(14).
+room(15).
+room(16).
+room(17).
+room(18).
+room(19).
+room(20).
+room(21).
 
 getUserInput(X):-
 	write('Choose a player'),
@@ -48,11 +71,17 @@ getWhoSuggest(X):-
 	nl,
 	read(X).
 
+getWhoSuggest(X):-
+	write("Who made the Accusation"),
+	nl,
+	read(X).
 
-getAccusation(Accusation,Accuser,Disprover):-
-	getWhoSuggest(Accuser),
-	character(Accuser),	
-	write("Enter a suggession in the format Weapon, suspect, room: "),
+
+
+getSuggestion(Suggestion,Suggester,Disprover):-
+	getWhoSuggest(Suggester),
+	character(Suggester),
+        write("Enter a suggession in the order weapon, suspect, room: "),
 	nl, 
 	write("What was the weapon"),
 	nl,
@@ -66,14 +95,14 @@ getAccusation(Accusation,Accuser,Disprover):-
 	nl,
 	read(Room),
 	room(Room),
-	Accusation = [Weapon,Suspect,Room],
+	Suggestion = [Weapon,Suspect,Room],
 	write("Who disproved the Accusation, if no one disproves it, enter the name of the Accuser"),
 	nl, 
 	read(Disprover),
-	character(Disprover).
+	character(Disprover).%replace with player(Disprover)
 
 getAccusation(Accusation,Accuser):-
-	write("Who made an incorrect accussation"),
+	write("Who made an icorrect accussation"),
 	nl,
 	read(Accuser),
 	character(Accuser),	
@@ -102,9 +131,11 @@ initGame(Characters,Hands):-
 	nl,
 	read(C1),
 	character(C1),
+        assert(playerCharacter(C1)),
 	write("How many cards are in your hand?"),
 	nl, 
 	read(H1),
+        assert(handSize(C1,H1)),
 	Characters = [C1|CT],
 	Hands = [H1|HT],
 	N1 is NPlayers - 1,
@@ -121,13 +152,34 @@ playerInfo(NPlayers,[CH|CT],[NH|NT]):-
 	write("How many cards does the player have in their hand?"),
 	nl,
 	read(NH),
+        assert(handSize(CH,NH)),
 	N1 is NPlayers - 1,
 	playerInfo(N1,CT,NT).
-	
-
 
 menuScreen(X):-
 	write("Press 1 to record a Accusation"),
 	nl,
 	write("Press 2 to record an accusation").
 
+:- dynamic handSize/2.
+% handSize(C,H) if character has H cards in hand.
+
+:- dynamic playerCharacter/1.
+% This character has the distinction of being controlled by the player.
+% They get to know the specific card which refutes their suggestion.
+
+
+:- dynamic recordedSuggestion/3.
+% A suggestion is ([Weapon,Suspect, Room], Suggester, Disprover)
+% the suggestion is a weapon suspect and Room
+% the suggester is the player that makes the suggestion
+% the disprover is the player that shows the suggester a card to disprove it
+% if the suggestion goes all the way around the table,
+% the Suggester and the Disprover are the same character
+
+:- dynamic recordedAccusation/3.
+% An accusation is like a suggestion, but without a disprover
+% All accusations inside the database are false.
+% this is because true accusations end the game.
+
+:- dynamic disprovedCards/2.
