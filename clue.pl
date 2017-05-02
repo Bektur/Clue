04161b0f1f817dd3d0b0f35bc327470d1dee592c
Bektur <tgbeko@gmail.com>
2017-05-02 02:51:14 -0700

    Added solution generator

--- clue.pl
+++ clue.pl
@@ -1,4 +1,3 @@
-
 % It's doable to create an interface using prolog 
 %Use swi-prolog manual
 %This is just list manipulation
@@ -8,10 +7,40 @@
 %6 weapons 
 %9 rooms 
 
+% this is to clear the dynamic variable every time we consult 
+% the game
+:- abolish(setofSolutions/1).
+
 % maybe create dynamic varaibles for which cards does the character have
 :- dynamic handSize/2.
-:- dynamic knownCards/1.
+% handSize(C,H) if character has H cards in hand.
+
+:- dynamic playerCharacter/1.
+% This character has the distinction of being controlled by the player.
+% They get to know the specific card which refutes their suggestion.
+
+
+:- dynamic recordedSuggestion/3.
+% A suggestion is ([Weapon,Suspect, Room], Suggester, Disprover)
+% the suggestion is a weapon suspect and Room
+% the suggester is the player that makes the suggestion
+% the disprover is the player that shows the suggester a card to disprove it
+% if the suggestion goes all the way around the table,
+% the Suggester and the Disprover are the same character
+
+:- dynamic failedAccusation/3.
+% An accusation is like a suggestion, but without a disprover
+% All accusations inside the database are false.
+% this is because true accusations end the game.
+
 :- dynamic disprovedCards/2.
+% disprovedCards(Character, card) if character is known to hold card
+% in their hand.
+
+:- dynamic setofSolutions/1.
+% we generate all the possible solutions for cards and save it in 
+% setofSolutions variable so that we can suggest the players 
+
 
 
 % List of all possible characters
@@ -22,12 +51,12 @@ character(white).
 character(plum).
 character(peacock).
 
-character(1).
-character(2).
-character(3).
-character(4).
-character(5).
-character(6).
+% character(1).
+% character(2).
+% character(3).
+% character(4).
+% character(5).
+% character(6).
 
 % List of all possible weapons
 weapon(knife).
@@ -37,12 +66,12 @@ weapon(revolver).
 weapon(wrench).
 weapon(pipe).
 
-weapon(7).
-weapon(8).
-weapon(9).
-weapon(10).
-weapon(11).
-weapon(12).
+% weapon(7).
+% weapon(8).
+% weapon(9).
+% weapon(10).
+% weapon(11).
+% weapon(12).
 
 % List of all possible rooms
 room(kitchen).
@@ -55,38 +84,61 @@ room(lounge).
 room(hall).
 room(study).
 
-room(13).
-room(14).
-room(15).
-room(16).
-room(17).
-room(18).
-room(19).
-room(20).
-room(21).
+% room(13).
+% room(14).
+% room(15).
+% room(16).
+% room(17).
+% room(18).
+% room(19).
+% room(20).
+% room(21).
 
 
+
+% generate all the available solutions of three cards and save them in the 
+% dynamic variable setofSolutions 
+generateSol(L):-
+	weapon(X),
+	L = [X|T],
+	generateChar(T),
+	assert(setofSolutions(L)),
+	generateSol(T).
+
+% get all the available characters
+generateChar(L):-
+	character(X),
+	L = [X|T],
+	generateRoom(T).
+
+%get all the available roooms
+generateRoom([]).
+generateRoom(T):-
+	room(X),
+	T = X. 
+
+printSolutions:-
+	generateSol(_);
+	findall(L,setofSolutions(L),L1),
+	printResults(L1).
+
+printResults([]).
+printResults([H|T]):-
+	write(H),
+	nl,
+	printResults(T).
 
 getWhoSuggest(X):-
 	write("Who made the Suggestion"),
 	nl,
-	read(X).
-
-getWhoSuggest(X):-
-	write("Who made the Accusation"),
-	nl,
-	read(X).
+	read(X),
+    nl.
 
 
-getSuggestion(Suggestion,Suggester,Disprover):-
-	getWhoSuggest(Suggester),
-	character(Suggester),	
-	write("Enter a suggession in the format weapon, suspect, room: "),
-
-getSuggestion(Suggestion,Suggester,Disprover):-
+getSuggestion(Suggestion, Suggester, Disprover, Pc):-
 	getWhoSuggest(Suggester),
 	character(Suggester),
-    write("Enter a suggession in the order weapon, suspect, room: "),
+        write("Enter a suggession in the order weapon, suspect, room: "),
 	nl, 
 	write("What was the weapon"),
 	nl,
@@ -104,30 +156,28 @@ getSuggestion(Suggestion,Suggester,Disprover):-
 	write("Who disproved the Suggestion, if no one disproves it, enter the name of the Suggester"),
 	nl, 
 	read(Disprover),
-	character(Disprover),
-	decideDisprover(Disprover).
-	
+	character(Disprover), %replace with player(Disprover)
+        assert(recordedSuggestion(Suggestion,Suggester,Disprover)),
+        (Pc = Suggester, Suggester \= Disprover -> 
+            write("What card did they show you?"),
+            nl,
+            read(X),
+            nl,
+            assert(disprovedCards(Disprover,X));
+            % if no one disproves the sugggestion then none of the players have 
+            % suggested cards
+            true
+        ).
 
 
-decideDisprover(Disprover):-
-	write("Write your card to disprove"),
-	nl, 
-	read(Card),
-	assert(disprovedCards(Disprover,Card).
-
-getAccusatio(Accusation,Accuser):-
-	write("Who made an incorrect accussation"),
-	write("Who disproved the Accusation, if no one disproves it, enter the name of the Accuser"),
-	nl, 
-	read(Disprover),
-	character(Disprover).%replace with player(Disprover)
-
-getAccusation(Accusation,Accuser):-
-	write("Who made an icorrect accussation"),
+%getAccusation.  Used when other players make an incorrect accusation
+% we do not record correct accusations, as those end the game.
+getAccusation():-
+	write("Who made an incorrect accusation"),
 	nl,
 	read(Accuser),
 	character(Accuser),	
-	write("Enter the incorrect Suggestion in the format weapon, suspect, room: "),
+	write("Enter the incorrect accusation in the format weapon, suspect, room: "),
 	nl, 
 	write("What was the weapon"),
 	nl,
@@ -141,19 +191,22 @@ getAccusation(Accusation,Accuser):-
 	nl,
 	read(Room),
 	room(Room),
-	Accusation = [Weapon,Suspect,Room].
+	Accusation = [Weapon,Suspect,Room],
+	assert(failedAccusation(Accusation, Accuser)).
 
 getHands(0,_).
 getHands(NHands,Character):-
-	write("Enter your cards"),
+	write("Enter the name of one of the cards in your hand."),
 	nl, 
 	read(Hands),
+        nl,
 	assert(handSize(Character,Hands)),
 	assert(disprovedCard(Character,Hands)),
 	N1 is NHands - 1,
 	getHands(N1,Character).
 
 
+%starts the game
 initGame(Characters,Hands):-
 	write("Enter the number of players"),
 	nl,
@@ -163,16 +216,21 @@ initGame(Characters,Hands):-
 	nl,
 	read(C1),
 	character(C1),
-        assert(playerCharacter(C1)),
 	write("How many cards are in your hand?"),
 	nl, 
 	read(H1),
-	getHands(H1,C1),
-    assert(handSize(C1,H1)),
+        nl,
+    	assert(handSize(C1,H1)),
+        %record the handsize of the player
 	Characters = [C1|CT],
 	Hands = [H1|HT],
 	N1 is NPlayers - 1,
-	playerInfo(N1,CT,HT).
+	playerInfo(N1,CT,HT),
+        %record the handsizes of others
+        getHands(H1,C1),!,
+        %return to the menu screen
+        menuScreen(C1).
+        
 
 % NH is head for hand size list
 playerInfo(0,[],[]).
@@ -189,31 +247,49 @@ playerInfo(NPlayers,[CH|CT],[NH|NT]):-
 	N1 is NPlayers - 1,
 	playerInfo(N1,CT,NT).
 
+% intersection predicate to decide the intersection of two lists 
+intersection([],_,[]).
+intersection([H1|T1],L2,[H1|T2]):-
+	member(H1,L2),
+	intersection(T1,L2,T2),
+	!.
 
-menuScreen(X):-
-	write("Press 1 to record a Suggestion"),
+intersection([_|T1],L2,T2):-
+	intersection(T1,L2,T2).
+
+
+
+%displays a menu screen.  the player selects an option from it.
+menuScreen(C1):-
+	write("Enter 1 to record a suggestion another player made"),
 	nl,
-	write("Press 2 to record an Suggestion").
+	write("Enter 2 to record an accusation another player made"),
+        nl,
+        write("Enter 3 to display the history of suggestions"),
+        nl,
+        write("Enter 4 to display the history of incorrect accusations"),
+        nl,
+        write("Enter 5 to display advice on what accusation to make"),
+        nl,
+        write("Enter 6 for advice on a suggestion to make"),
+        nl,
+        write("Enter 7 if you would like to exit"),
+        nl,
+        read(S),
+        (S = 1 -> getSuggestion(Suggestion,Suggester,Disprover,C1);
+         S = 2 -> getAccusation;
+         S = 3 -> displayHistory;
+         S = 4 -> displayAccHist;
+         S = 5 -> calculateAccusation;
+         S = 6 -> calculateSuggestion;
+         S = 7 -> halt;
+         write("not a valid selection."),menuScreen(C1)
+         ),
+         menuScreen(C1).
 
-:- dynamic handSize/2.
-% handSize(C,H) if character has H cards in hand.
-
-:- dynamic playerCharacter/1.
-% This character has the distinction of being controlled by the player.
-% They get to know the specific card which refutes their suggestion.
+calculateAccusation.
+calculateSuggestion.
 
 
-:- dynamic recordedSuggestion/3.
-% A suggestion is ([Weapon,Suspect, Room], Suggester, Disprover)
-% the suggestion is a weapon suspect and Room
-% the suggester is the player that makes the suggestion
-% the disprover is the player that shows the suggester a card to disprove it
-% if the suggestion goes all the way around the table,
-% the Suggester and the Disprover are the same character
-
-:- dynamic recordedAccusation/3.
-% An accusation is like a suggestion, but without a disprover
-% All accusations inside the database are false.
-% this is because true accusations end the game.
-
-:- dynamic disprovedCards/2.
+displayHistory :-
+       recordedSuggestion(X,Y,Z),format('~w ~w ~w', [X, Y, Z]).
