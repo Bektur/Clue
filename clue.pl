a378d9ee48af8729130f61097dcfde9185d41732
Bektur <tgbeko@gmail.com>
2017-05-02 02:55:00 -0700

    clean bugs

--- clue.pl
+++ clue.pl
@@ -129,7 +129,6 @@ printResults([]).
 printResults([H|T]):-
 	write(H),
 	nl,
-<<<<<<< HEAD
 	printResults(T).
 
 getWhoSuggest(X):-
@@ -139,13 +138,11 @@ getWhoSuggest(X):-
     nl.
 
 
-=======
 	read(X),
         nl.
 
 
 
->>>>>>> origin/master
 getSuggestion(Suggestion, Suggester, Disprover, Pc):-
 	getWhoSuggest(Suggester),
 	character(Suggester),
@@ -175,11 +172,8 @@ getSuggestion(Suggestion, Suggester, Disprover, Pc):-
             read(X),
             nl,
             assert(disprovedCards(Disprover,X));
-<<<<<<< HEAD
             % if no one disproves the sugggestion then none of the players have 
             % suggested cards
-=======
->>>>>>> origin/master
             true
         ).
 
@@ -268,14 +262,12 @@ intersection([H1|T1],L2,[H1|T2]):-
 	intersection(T1,L2,T2),
 	!.
 
-<<<<<<< HEAD
 intersection([_|T1],L2,T2):-
 	intersection(T1,L2,T2).
 
 
 
-=======
->>>>>>> origin/master
+
 %displays a menu screen.  the player selects an option from it.
 menuScreen(C1):-
 	write("Enter 1 to record a suggestion another player made"),
